392b82aa48f200bcac15ff7ed6a38d24ef379b0b
Yomi Olutimehin <yomi.olutimehin@yahoo.com>
2021-04-28 20:23:16 +0000

    updates post review comments_1

--- bash scripts/deploy.sh
+++ bash scripts/deploy.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 docker login --username=$HEROKU_LOGIN --password=$HEROKU_API_KEY registry.heroku.com
+docker build --target production --tag olutiab/my-heroku-build .
 docker tag olutiab/my-heroku-build registry.heroku.com/olutiab-todo-app/web
-
-docker push registry.heroku.com/olutiab-todo-app/web
-
 docker push registry.heroku.com/olutiab-todo-app/web
 heroku container:release web --app olutiab-todo-app
