e40f9c9e294f92683f02dbe5b69c70c3adec53ca
Yomi Olutimehin <yomi.olutimehin@yahoo.com>
2021-04-28 09:01:06 +0000

    updated deploy script

--- bash scripts/deploy.sh
+++ bash scripts/deploy.sh
@@ -1,3 +1,3 @@
-
-      - docker push registry.heroku.com/olutiab-todo-app/web
-      - heroku container:release web --app olutiab-todo-app
+#!/usr/bin/env bash
+docker push registry.heroku.com/olutiab-todo-app/web
+heroku container:release web --app olutiab-todo-app
